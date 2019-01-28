e7851bf58cc3a07f24e7050d15033d36b6708830
chemica1 <46700473+chemica1@users.noreply.github.com>
2019-01-28 15:55:28 +0900

    Initial commit

---
+++ src/components/PhoneForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+    render() {
+        return (
+            <div>
+               phoneForm 
+            </div>
+        );
+    }
+}
+
+export default PhoneForm;phoneForm
